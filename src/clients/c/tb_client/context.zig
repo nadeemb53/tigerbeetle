33a1cedf5a046d7e97a0d066eb9ff376a44202fd
Alex Kladov <aleksey.kladov@gmail.com>
2026-03-02 14:11:47 +0000

    wip

--- src/clients/c/tb_client/context.zig
+++ src/clients/c/tb_client/context.zig
@@ -323,6 +323,8 @@ pub fn ContextType(
                     .aof_recovery = false,
                     .message_bus_options = .{
                         .configuration = context.addresses.const_slice(),
+                        .discovery = false,
+                        .listen = null,
                         .io = &context.io,
                         .trace = null,
                     },
--- src/stdx/flags.zig
+++ src/stdx/flags.zig
@@ -353,7 +353,6 @@ fn parse_value(comptime T: type, flag: []const u8, value: [:0]const u8) T {
     if (V == []const u8 or V == [:0]const u8) return value;
     if (V == bool) return parse_value_bool(flag, value);
     if (@typeInfo(V) == .int) return parse_value_int(V, flag, value);
-    if (@typeInfo(V) == .@"enum") return parse_value_enum(V, flag, value);
     if (@hasDecl(V, "parse_flag_value")) {
 
         // Contracts:
@@ -380,6 +379,7 @@ fn parse_value(comptime T: type, flag: []const u8, value: [:0]const u8) T {
             },
         }
     }
+    if (@typeInfo(V) == .@"enum") return parse_value_enum(V, flag, value);
     comptime unreachable;
 }
 
